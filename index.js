314d66d4b3dd53fb0b03aa1c721af8d07364c7f4
smithwebtek <brad@smithwebtek.com>
2017-10-17 10:51:02 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+// function addComment() {
+//   var commentDiv = document.getElementById('comments');
+//   commentDiv.innerHTML = '<div><p>This research is bold and important!</p><p>I wish to join your team</p></div><div><p>posted by: <span>I swear I am not a woodchuck</span></p></div>';
+// } 
+
+function addComment(){
+  event.preventDefault()
+  let bodyText = document.getElementById('commentText').value
+  let commenter = document.getElementById('commenterName').value
+  let commentBody = createCommentBody(bodyText)
+  let commenterLabel = createCommenterLabel(commenter)
+  postNewComment(commentBody, commenterLabel)
+}
+
+function createCommentBody(comment){
+  let bodyDiv = document.createElement('div')
+  let bodyPara = document.createElement('p')
+  bodyPara.innerHTML = comment 
+  bodyDiv.appendChild(bodyPara)
+  return bodyDiv
+}
+
+function createCommenterLabel(commenter){
+  let commenterDiv = document.createElement('div')
+  let commenterLabel = document.createElement('p')
+    commenterLabel.innerHTML = "posted by:@"
+  let commenterName = document.createElement('span')
+    commenterName.innerHTML = commenter
+    commenterLabel.appendChild(commenterName)
+    commenterDiv.appendChild(commenterLabel)
+    return commenterDiv
+}
+
+function postNewComment(body, commenter){
+  let commentsDiv = document.getElementById('comments')
+  let newCommentDiv = document.createElement('div')
+  newCommentDiv.appendChild(body)
+  newCommentDiv.appendChild(commenter)
+  commentsDiv.appendChild(newCommentDiv)
+}
