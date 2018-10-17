5aec3fdfc27cac8f8678e3fcbf19f32fd95eca73
smithwebtek <brad@smithwebtek.com>
2018-10-16 22:51:28 -0400

    Update lab

--- index.js
+++ index.js
@@ -1,42 +1,52 @@
 // function addComment() {
 //   var commentDiv = document.getElementById('comments');
-//   commentDiv.innerHTML = '<div><p>This research is bold and important!</p><p>I wish to join your team</p></div><div><p>posted by: <span>I swear I am not a woodchuck</span></p></div>';
+//   commentDiv.innerHTML = '
+//		<div>
+// 			<p>This research is bold and important!</p>
+// 			<p>I wish to join your team</p>
+//		</div>
+// 		<div>
+// 			<p>posted by: <span>I swear I am not a woodchuck</span></p>
+// 		</div>
+//		';
 // } 
 
-function addComment(){
-  event.preventDefault()
-  // let bodyText = document.getElementById('commentText').value
-  // let commenter = document.getElementById('commenterName').value
-  let bodyText = event.srcElement.commentText.value 
-  let commenter = event.srcElement.commenterName.value 
-  let commentBody = createCommentBody(bodyText)
-  let commenterLabel = createCommenterLabel(commenter)
-  postNewComment(commentBody, commenterLabel)
+function addComment() {
+	event.preventDefault()
+	// let bodyText = document.getElementById('commentText').value
+	// let commenter = document.getElementById('commenterName').value
+	let bodyText = event.srcElement.commentText.value
+	let commenter = event.srcElement.commenterName.value
+
+	let commentBody = createCommentBody(bodyText)
+	let commenterLabel = createCommenterLabel(commenter)
+	postNewComment(commentBody, commenterLabel)
 }
 
-function createCommentBody(comment){
-  let bodyDiv = document.createElement('div')
-  let bodyPara = document.createElement('p')
-  bodyPara.innerHTML = comment 
-  bodyDiv.appendChild(bodyPara)
-  return bodyDiv
+function createCommentBody(comment) {
+	let bodyDiv = document.createElement('div')
+	let bodyPara = document.createElement('p')
+	bodyPara.innerHTML = comment
+	bodyDiv.appendChild(bodyPara)
+	return bodyDiv
 }
 
-function createCommenterLabel(commenter){
-  let commenterDiv = document.createElement('div')
-  let commenterLabel = document.createElement('p')
-    commenterLabel.innerHTML = "posted by:@"
-  let commenterName = document.createElement('span')
-    commenterName.innerHTML = commenter
-    commenterLabel.appendChild(commenterName)
-    commenterDiv.appendChild(commenterLabel)
-    return commenterDiv
+function createCommenterLabel(commenter) {
+	let commenterDiv = document.createElement('div')
+	let commenterLabel = document.createElement('p')
+	commenterLabel.innerHTML = '<hr>'
+	commenterLabel.innerHTML = commenterLabel.innerHTML + "posted by:@"
+	let commenterName = document.createElement('span')
+	commenterName.innerHTML = commenter
+	commenterLabel.appendChild(commenterName)
+	commenterDiv.appendChild(commenterLabel)
+	return commenterDiv
 }
 
-function postNewComment(body, commenter){
-  let commentsDiv = document.getElementById('comments')
-  let newCommentDiv = document.createElement('div')
-  newCommentDiv.appendChild(body)
-  newCommentDiv.appendChild(commenter)
-  commentsDiv.appendChild(newCommentDiv)
+function postNewComment(body, commenter) {
+	let commentsDiv = document.getElementById('comments')
+	let newCommentDiv = document.createElement('div')
+	newCommentDiv.appendChild(body)
+	newCommentDiv.appendChild(commenter)
+	commentsDiv.appendChild(newCommentDiv)
 }
